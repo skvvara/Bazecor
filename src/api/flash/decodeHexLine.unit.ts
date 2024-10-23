d8df85a40f84c2d3da81996069857d76c0b4549e
Lawrence Brooks <84.lbrooks@gmail.com>
2024-10-23 17:07:22 -0400

    Improve test coverage, documentation, identify edge cases
    Signed-off-by: Lawrence Brooks <84.lbrooks@gmail.com>

--- src/api/flash/decodeHexLine.unit.ts
+++ src/api/flash/decodeHexLine.unit.ts
@@ -2,14 +2,68 @@ import { describe, expect, test } from "vitest";
 import { decodeHexLine } from "./decodeHexLine";
 
 describe(`${decodeHexLine.name}()`, () => {
-  test("decode hex line", () => {
-    const data = "AB1234";
-    const i = `03123401${data}`;
-    const r = decodeHexLine(i);
-    expect(r.str).toEqual(i);
-    expect(r.len).toEqual(3);
-    expect(r.address).toEqual(0x1234);
-    expect(r.type).toEqual(1);
-    expect(r.data).toEqual(Uint8Array.from([0xab, 0x12, 0x34]));
+  test.each([
+    {
+      line: "03123401AB1234",
+      expected: {
+        str: "03123401AB1234",
+        len: 3,
+        address: 0x1234,
+        type: 1,
+        data: Uint8Array.from([0xab, 0x12, 0x34]),
+      },
+    },
+    {
+      line: "",
+      expected: {
+        str: "",
+        len: NaN,
+        address: NaN,
+        type: NaN,
+        data: Uint8Array.from([]),
+      },
+    },
+    {
+      line: "03",
+      expected: {
+        str: "03",
+        len: 3,
+        address: NaN,
+        type: NaN,
+        data: Uint8Array.from([]),
+      },
+    },
+    {
+      line: "031234",
+      expected: {
+        str: "031234",
+        len: 3,
+        address: 0x1234,
+        type: NaN,
+        data: Uint8Array.from([]),
+      },
+    },
+    {
+      line: "03123401",
+      expected: {
+        str: "03123401",
+        len: 3,
+        address: 0x1234,
+        type: 1,
+        data: Uint8Array.from([]),
+      },
+    },
+    {
+      line: "03123401AB",
+      expected: {
+        str: "03123401AB",
+        len: 3,
+        address: 0x1234,
+        type: 1,
+        data: Uint8Array.from([0xab]),
+      },
+    },
+  ])(`input: '$line'`, ({ line, expected }) => {
+    expect(decodeHexLine(line)).toStrictEqual(expected);
   });
 });
